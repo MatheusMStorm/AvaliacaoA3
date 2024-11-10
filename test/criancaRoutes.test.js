f333412dc5dfd3041c94964744abda2e824f6567
Moura <matheus.moura0308@gmail.com>
2024-11-10 10:31:20 -0300

    test: adiciona linha de teste ao teste de integração

--- test/criancaRoutes.test.js
+++ test/criancaRoutes.test.js
@@ -1,22 +1,29 @@
 const request = require('supertest');
 const app = require('../src/app');
+const db = require('../src/config/conexao')
 
 describe('Testes de Integração para Rotas de Criança', () => {
-  test('GET /api/criancas deve retornar lista de crianças', async () => {
-    const response = await request(app).get('/api/criancas');
-    expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true);
-  });
+    const rgCrianca = '20202020';
 
-  test('POST /api/criancas deve criar uma nova criança', async () => {
-    const novaCrianca = {
-      rg_crianca: '10101010',
-      nome_crianca: 'Marcos',
-      idade_crianca: 12,
-      data_nasc: '2012-05-01'
-    };
-    const response = await request(app).post('/api/criancas').send(novaCrianca);
-    expect(response.status).toBe(201);
-    expect(response.body.nome_crianca).toBe('Marcos');
-  });
+    test('GET /api/criancas deve retornar lista de crianças', async () => {
+        const response = await request(app).get('/api/criancas');
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+    });
+
+    test('POST /api/criancas deve criar uma nova criança', async () => {
+        const novaCrianca = {
+            rg_crianca: rgCrianca,
+            nome_crianca: 'Marcos',
+            idade_crianca: 12,
+            data_nasc: '2012-05-01'
+        };
+        const response = await request(app).post('/api/criancas').send(novaCrianca);
+        expect(response.status).toBe(201);
+        expect(response.body.nome_crianca).toBe('Marcos');
+    });
+
+    afterEach(async () => {
+        await db.query('DELETE FROM projeto_iessa.crianca WHERE rg_crianca = $1', [rgCrianca]);
+    });
 });
