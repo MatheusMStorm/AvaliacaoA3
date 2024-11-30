6fe57d886d370d245d86aa325096d2584776e854
Moura <matheus.moura0308@gmail.com>
2024-11-29 22:12:28 -0300

    chore: altera porta de conexão da API e privatiza dados do BD

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,11 +1,16 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres.wfzstojledyyzaqwdcem',
-  host: 'aws-0-sa-east-1.pooler.supabase.com',
-  database: 'postgres',
-  password: 'AvaliacaoA3',
-  port: 6543,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+pool.on('error', () => {
+  console.log('Erro na conexão com o banco de dados.');
 });
 
 module.exports = pool;
