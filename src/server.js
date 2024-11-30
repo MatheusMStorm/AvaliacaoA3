dc190e609932b80bd0b1c06b95535667e92195c6
Moura <matheus.moura0308@gmail.com>
2024-11-29 22:16:26 -0300

    refactor: remove a função app.get do server

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 const app = require('./app');
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("API rodando!");
-});
-
 app.listen(port, () => {
   console.log(`API rodando na porta ${port}`);
 });
